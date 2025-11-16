5b5ae5f5a886cfde999a5cedd418d35001e86959
Gustavo Álamo Barea <ga@fnxs.de>
2025-11-16 22:00:55 +0100

    Actualización estética de website

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const overlay = document.getElementById("gif-carga");
+
+  document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      overlay.style.display = "flex";
+
+      const href = this.href;
+
+      setTimeout(() => {
+        window.location.href = href;
+      }, 1000);
+    });
+  });
+});
